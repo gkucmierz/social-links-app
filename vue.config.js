5809da40a03e55cf038f966391892b8c341cb85d
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-09-27 18:58:35 +0200

    add workflow & vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  lintOnSave: false
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/social-links-app/' : '/',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Social-links module demo',
+    }
+  },
+  lintOnSave: false,
+});
