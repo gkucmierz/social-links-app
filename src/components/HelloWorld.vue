e42ef44a709cc51f21ccb6cb41d2e11156581464
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-09-27 18:35:25 +0200

    vue create

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h3>Demo app that uses utilizes social-links npm module</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
